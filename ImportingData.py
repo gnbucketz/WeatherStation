dc98a1e4b9f7b2d8d16c223324bbe87ab56a0d80
gnbucketz <143123145+gnbucketz@users.noreply.github.com>
2023-10-22 15:23:05 -0700

    Update ImportingData.py

--- ImportingData.py
+++ ImportingData.py
@@ -1,4 +1,5 @@
 import serial
+import datetime
 
 # Open the serial port
 ser = serial.Serial('COM3', 9600) 
@@ -7,6 +8,7 @@ try:
     while True:
         # Read a line of data from the Arduino
         data = ser.readline().decode('utf-8').strip()
-        print(data) 
+        timestamp = datetime.datetime.now().strftime("%m/%d/%Y-%I:%M:%S %p")
+        print(f"{timestamp} - {data}") 
 except KeyboardInterrupt:
     ser.close()
