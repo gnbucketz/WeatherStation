35cf44893f59c6983875b454ff0c8a22cdc9ed49
gnbucketz <143123145+gnbucketz@users.noreply.github.com>
2023-10-22 15:05:05 -0700

    Update ImportingData.py

--- ImportingData.py
+++ ImportingData.py
@@ -1,20 +1,12 @@
 import serial
-import tkinter as tk
 
+# Open the serial port
 ser = serial.Serial('COM3', 9600) 
-root = tk.Tk()
-
-root.title("Weather Station")
-root.configure(bg="lightgray")
-temperature_label = tk.Label(root, text="The current ", bg="lightgray")
-humidity_label = tk.Label(root, text="The current ", bg="lightgray")
-temperature_entry = tk.Entry(root)
-humidity_entry = tk.Entry(root)
 
 try:
     while True:
         # Read a line of data from the Arduino
         data = ser.readline().decode('utf-8').strip()
-        print(data)  
+        print(data) 
 except KeyboardInterrupt:
     ser.close()
